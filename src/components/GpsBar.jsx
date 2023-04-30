531d056227833462a1f2ddde8fd989de8dc275a1
Ismail Courr <contact@ismailcourr.ml>
2023-04-30 06:13:03 +0100

    Fix image

--- src/components/GpsBar.jsx
+++ src/components/GpsBar.jsx
@@ -30,7 +30,7 @@ const GpsBar = () => {
     <div className="flex flex-col items-center justify-center w-full h-16 bg-[#D9D9D9] font-merriweatherSans font-bold">
       <p className="text-gray-600 text-xs">GPS</p>
       <button className={`${gps ? 'bg-green-500 flex-row-reverse' : 'bg-[#F69E1A]'} flex justify-between items-center p-1 w-24 rounded-full duration-100`} onClick={handleGps}>
-        <img src="./img/gps.png" alt="gps" className="bg-gray-300 rounded-full" />
+        <img src="/img/gps.png" alt="gps" className="bg-gray-300 rounded-full" />
         <p className={`${gps ? 'block' : 'hidden'} pl-2`}>ON</p>
         <p className={`${gps ? 'hidden' : 'block'} pr-2`}>OFF</p>
       </button>
