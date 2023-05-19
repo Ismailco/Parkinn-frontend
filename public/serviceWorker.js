70012167e65f7b4228aac92318e84a6e96e313a0
Ismail Courr <contact@ismailcourr.ml>
2023-05-19 19:22:06 +0100

    Fix service workers

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,9 +1,13 @@
 const CACHE_NAME = 'packinn-cache-v1';
-const urlsToCache = ['../dist/index.html', '../dist/logo.svg', '../dist/assets/apple-icon-180-e27d7f0f.png', '../dist/assets/index-2443fa3f.css', '../dist/assets/index-79c974eb.js', '../dist/icons/apple-icon-180.png', '../dist/icons/manifest-icon-192.maskable.png', '../dist/icons/manifest-icon-512.maskable.png', '../dist/img/gps.png', '../dist/img/home.png', '../dist/img/location.png', '../dist/img/NavLogo.svg', '../dist/img/parkingIcon.png'];
-/* eslint-disable no-undef */
-globalThis.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
-});
+const CACHE = 'pwabuilder-offline-page';
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const offlineFallbackPage = ['../dist/index.html', '../dist/logo.svg', '../dist/assets/apple-icon-180-e27d7f0f.png', '../dist/assets/index-2443fa3f.css', '../dist/assets/index-79c974eb.js', '../dist/icons/apple-icon-180.png', '../dist/icons/manifest-icon-192.maskable.png', '../dist/icons/manifest-icon-512.maskable.png', '../dist/img/gps.png', '../dist/img/home.png', '../dist/img/location.png', '../dist/img/NavLogo.svg', '../dist/img/parkingIcon.png'];
+
+// globalThis.addEventListener('install', (event) => {
+//   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(offlineFallbackPage)));
+// });
 globalThis.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -14,18 +18,12 @@ globalThis.addEventListener('fetch', (event) => {
     })
   );
 });
-globalThis.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
-  globalThis.skipWaiting();
-});
-
-const CACHE = 'pwabuilder-offline-page';
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// globalThis.addEventListener('install', (event) => {
+//   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(offlineFallbackPage)));
+//   globalThis.skipWaiting();
+// });
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = 'ToDo-replace-this-name.html';
-
 globalThis.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     globalThis.skipWaiting();
@@ -69,20 +67,3 @@ globalThis.addEventListener('fetch', (event) => {
     );
   }
 });
-
-// const CACHE = 'pwabuilder-offline';
-
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// globalThis.addEventListener('message', (event) => {
-//   if (event.data && event.data.type === 'SKIP_WAITING') {
-//     globalThis.skipWaiting();
-//   }
-// });
-
-// workbox.routing.registerRoute(
-//   new RegExp('/*'),
-//   new workbox.strategies.StaleWhileRevalidate({
-//     cacheName: CACHE,
-//   })
-// );
