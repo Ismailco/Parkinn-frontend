9e8ed4c872cffd9a13e97e176fe1ba3b99d91d5a
Ismail Courr <contact@ismailcourr.ml>
2023-05-19 19:30:48 +0100

    Remove redandent code

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -5,25 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const offlineFallbackPage = ['../dist/index.html', '../dist/logo.svg', '../dist/assets/apple-icon-180-e27d7f0f.png', '../dist/assets/index-2443fa3f.css', '../dist/assets/index-79c974eb.js', '../dist/icons/apple-icon-180.png', '../dist/icons/manifest-icon-192.maskable.png', '../dist/icons/manifest-icon-512.maskable.png', '../dist/img/gps.png', '../dist/img/home.png', '../dist/img/location.png', '../dist/img/NavLogo.svg', '../dist/img/parkingIcon.png'];
 
-globalThis.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(offlineFallbackPage)));
-});
-globalThis.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-globalThis.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(offlineFallbackPage)));
-  globalThis.skipWaiting();
-});
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+// PWABuilder service worker code to support offline functionality
 globalThis.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     globalThis.skipWaiting();
